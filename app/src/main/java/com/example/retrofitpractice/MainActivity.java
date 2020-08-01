8b912f6ef130aba1643b5c11d0ed8b3b6780c5b1
PKS <pkumarsaha71@gmail.com>
2020-08-01 17:45:29 +0600

    the json data was toasted

--- app/src/main/java/com/example/retrofitpractice/MainActivity.java
+++ app/src/main/java/com/example/retrofitpractice/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.retrofitpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.retrofitpractice.Model.Products;
 import com.example.retrofitpractice.Model.retrofit.ApiInterface;
 import com.example.retrofitpractice.Model.retrofit.RetrofitClient;
+import com.google.gson.Gson;
 
 import java.util.List;
 
@@ -24,16 +26,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         apiInterface = RetrofitClient.getRetrofitInstance().create(apiInterface.getClass());
+        //fetch lists
         Call<List<Products>> call = apiInterface.getAllProduct();
+
+        // Execute the call asynchronously. Get a positive or negative callback.
         call.enqueue(new Callback<List<Products>>() {
             @Override
             public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
-                
+
+                // The network call was a success and we got a response
+                //TODO: use the repository list and display it
+                List<Products>s = response.body();
+                //showing the detch data
+                Gson gson = new Gson();
+                String s1 = gson.toJson(s);
+                Toast.makeText(getApplicationContext(),s1,Toast.LENGTH_SHORT).show();
+
+
             }
 
             @Override
             public void onFailure(Call<List<Products>> call, Throwable t) {
 
+                // the network call was a failure
+                // TODO: handle error
             }
         });
     }
--- app/src/main/java/com/example/retrofitpractice/Model/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/retrofitpractice/Model/retrofit/RetrofitClient.java
@@ -11,7 +11,7 @@ public class RetrofitClient {
 
     public static Retrofit getRetrofitInstance() {
 
-        if(retrofit != null){
+        if(retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(API_BASE_URL).addConverterFactory(
                     GsonConverterFactory.create()
             ).build();
