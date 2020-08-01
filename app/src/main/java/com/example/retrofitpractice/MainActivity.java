decac6a04ab191d833e2b64f7c4039c6f14320e1
PKS <pkumarsaha71@gmail.com>
2020-08-01 17:31:07 +0600

    retrofit class is handled

--- app/src/main/java/com/example/retrofitpractice/MainActivity.java
+++ app/src/main/java/com/example/retrofitpractice/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.retrofitpractice.Model.Products;
+import com.example.retrofitpractice.Model.retrofit.ApiInterface;
+import com.example.retrofitpractice.Model.retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiInterface = RetrofitClient.getRetrofitInstance().create(apiInterface.getClass());
+        Call<List<Products>> call = apiInterface.getAllProduct();
+        call.enqueue(new Callback<List<Products>>() {
+            @Override
+            public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<List<Products>> call, Throwable t) {
+
+            }
+        });
     }
 }
